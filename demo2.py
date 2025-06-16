ba04956cf2f11d3529ec9fa1e90a612b1d41a887
TieuLenh <hieukaito2k5@gmail.com>
2025-06-16 22:44:42 +0700

    fix demo2

--- demo2.py
+++ demo2.py
@@ -7,6 +7,6 @@ screen = pygame.display.set_mode((wd_w,wd_h), pygame.RESIZABLE)
 running = True
 while running:
   for event in pygame.event.get():
-    if event.type() == pygame.QUIT:
+    if event.type == pygame.QUIT:
       running = False
 pygame.quit()

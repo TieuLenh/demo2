22f8f94a0e4b960cf3232ce0aa8aea8b97260fe3
TieuLenh <hieukaito2k5@gmail.com>
2025-06-16 22:49:48 +0700

    update demo21

---
+++ demo21.py
@@ -0,0 +1,10 @@
+from random import randint
+
+red = (250,0,0)
+green = (0,250,0)
+blue = (0,0,250)
+white = (250,250,250)
+black = (0,0,0)
+
+def randCl():
+    return (randint(0,250),randint(0,250),randint(0,250))

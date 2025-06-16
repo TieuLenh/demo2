3b156c3f10c471d6154d64b93cf55063eee19486
TieuLenh <hieukaito2k5@gmail.com>
2025-06-16 22:52:29 +0700

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,4 +1,5 @@
 import pygame
+import demo21 as dm21
 pygame.init()
 
 wd_w, wd_h = 600, 350
@@ -6,7 +7,9 @@ screen = pygame.display.set_mode((wd_w,wd_h), pygame.RESIZABLE)
 
 running = True
 while running:
+  sceen.fill(dm21.randCl())
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+  pygame.display.update()
 pygame.quit()

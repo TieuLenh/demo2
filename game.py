3f950f1375585688865a72209592a0c275418c99
TieuLenh <hieukaito2k5@gmail.com>
2025-06-22 23:09:38 +0700

    ranh lam sau

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ unit = 30
 #display setting
 dts =  pygame.display.get_desktop_sizes()
 dt_w, dt_h = dts[0]
-wd_w, wd_h = 12 * unit, 20*unit
+wd_w, wd_h = 20*unit, 12 * unit 
 screen = pygame.display.set_mode((wd_w, wd_h), flags=pygame.RESIZABLE)
 #surface
 sf1 = pygame.Surface((wd_w, wd_h))
@@ -16,43 +16,63 @@ sf1 = pygame.Surface((wd_w, wd_h))
 pl1 = player(w=unit,h=unit)
 pl1r = pl1.plr()
 print(pl1r)
-pl_step = 2
+pl_speed = 5
 
-#limited moving
+#player's moving
 def lim1(plr,w,h):
   if plr.x <= 0:
     plr.x = 0
   if plr.y <= 0:
     plr.y = 0
+  if plr.x >= w - plr.w:
+    plr.x = w - plr.w
+  if plr.y >= h - plr.h:
+    plr.y = h - plr.h
 
 #game loop
 clock = pygame.time.Clock()
 fps = 90
+t = 0
+fos = t/fps
+jump = 0
+k = 0
 running = True
 while running:
   if wd_w != screen.get_width() or wd_h != screen.get_height():
      wd_w, wd_h = pygame.display.get_window_size()
   
   #processing surface
-  screen.blit(sf1,(wd_w//2 - sf1.get_width()//2,0))
-  #screen.blit(sf1,(0,0))
+  screen.blit(sf1,(wd_w//2 - sf1.get_width()//2, wd_h//2 - sf1.get_height()//2,))
   sf1.fill(white) 
   #processing event.
   for event in pygame.event.get():
       if event.type == pygame.QUIT:
           running = False
-  key = pygame.key.get_pressed()
-  if key[pygame.K_w]:
-    pl1r.y -= pl_step
-  if key[pygame.K_s]:
-    pl1r.y += pl_step
-  if key[pygame.K_a]:
-    pl1r.x -= pl_step
-  if key[pygame.K_d]:
-    pl1r.x += pl_step
-  lim1(pl1r, wd_w, wd_h)
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE and jump == 0 and k == 0:
+          jump = 1
+          k = fps/2
+  if fos % 2 == 0:
+    key = pygame.key.get_pressed()
+    if jump == 1:
+      pl1r.y -= pl_speed
+      k -= 1
+    else:
+      pl1r.y += pl_speed
+    if k == 0:
+      jump = 0
+    if key[pygame.K_a]:
+      pl1r.x -= pl_speed
+    if key[pygame.K_d]:
+      pl1r.x += pl_speed
+  lim1(pl1r, sf1.get_width(), sf1.get_height())
   pygame.draw.rect(surface=sf1, color=red, rect=pl1r)
   #setting fps and display
   pygame.display.update()
   clock.tick(fps)
+  
+  t += 1000000000
+  if fos / fps >= 1:
+    fos = 0
+    fos += 1
 pygame.quit()

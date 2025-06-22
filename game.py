82818ef8371fff2e2855e63eea58da605084d6a1
TieuLenh <hieukaito2k5@gmail.com>
2025-06-22 14:10:17 +0700

    ok1

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
-from color import * 
+from color import *
+from object_game import player
 
 pygame.init()
 unit = 30
@@ -7,20 +8,30 @@ unit = 30
 #display setting
 wd_w, wd_h = 12 * unit, 20*unit
 screen = pygame.display.set_mode((wd_w, wd_h), flags=pygame.RESIZABLE)
+#surface
+sf1 = pygame.Surface((wd_w, wd_h))
 #player
-
+pl1 = player(w=unit,h=unit)
+pl1r = pl1.plr()
+pl_step = 1
 #game loop
 clock = pygame.time.Clock()
 fps = 90
 running = True
 while running:
-    
-    #processing event.
-    for event in pygame.event.get():
-        pass
-    
-    pygame.draw
-    #setting fps and display
-    pygame.display.update()
-    clock.tick(fps)
+  if wd_w != screen.get_width() or wd_h != screen.get_height():
+     wd_w, wd_h = pygame.display.get_window_size()
+  
+  #processing surface
+  screen.blit(sf1,(wd_w//2 - sf1.get_width()//2,0))
+  sf1.fill(green) 
+  #processing event.
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          running = False
+  pl1r = player.moving(pl1)
+  pygame.draw.rect(surface=sf1, color=red, rect=pl1r)
+  #setting fps and display
+  pygame.display.update()
+  clock.tick(fps)
 pygame.quit()
--- object_game.py
+++ object_game.py
@@ -1,1 +1,27 @@
 from random import randint
+import pygame
+class player(object):
+    def __init__(self, x = 0, y = 0, w = 0, h = 0):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def plr(self):
+        return pygame.Rect(self.x, self.y, self.w, self.h)
+    
+    def moving(self, wd_w = 0, wd_h = 0, step = 0, mode = 'flat'):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_d]:
+            self.x += step
+        if key[pygame.K_a]:
+            self.x -= step
+        if mode == 'flat':
+            if key[pygame.K_s]:
+                self.y += step
+            if key[pygame.K_w]:
+                self.y -= step
+        else:
+            if key[pygame.K_SPACE]:
+                self.y += 4 * step
+        return pygame.Rect(self.x, self.y, self.w, self.h)
